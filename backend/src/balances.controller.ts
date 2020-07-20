a0a75faf6358515265d0f5456d0f2bb07d9fbbfb
Agnieszka Olszewska <agnieszka.olszewska@brightinventions.pl>
2020-07-20 21:27:11 +0200

    Use PromiseContract from api-contract

--- backend/src/balances.controller.ts
+++ backend/src/balances.controller.ts
@@ -5,7 +5,7 @@ import { BalancesService } from './balances.service';
 const accounts = {
   ALICE: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
   BOB: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
-  CHARLIE: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+  CHARLIE: '5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y',
   DAVE: '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy',
   EVE: '5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw',
 }
--- backend/src/balances.service.ts
+++ backend/src/balances.service.ts
@@ -1,19 +1,20 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ApiPromise, Keyring, SubmittableResult, WsProvider } from '@polkadot/api';
-import { Abi } from '@polkadot/api-contract';
+import { Abi, PromiseContract as ApiContract } from '@polkadot/api-contract';
+import { ContractCallOutcome } from '@polkadot/api-contract/types';
 import { KeyringPair } from '@polkadot/keyring/types';
 import metadata from "./metadata.json";
 
-
 const SUBSTRATE_URL = 'ws://127.0.0.1:9944'
 const ERC20 = '5DhP1rd5AEZCeZY77Zttbt293rX6tX4QnqEajEMd5i1QKsnB'
-
+const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'
 
 @Injectable()
 export class BalancesService implements OnModuleInit {
   private api: ApiPromise;
   private abi: Abi;
   private alice: KeyringPair;
+  private apiContract: ApiContract;
 
   async onModuleInit() {
     Logger.log('Connecting to substrate chain...');
@@ -24,6 +25,7 @@ export class BalancesService implements OnModuleInit {
 
     const abiJSONobj = (<any>metadata);
     this.abi = new Abi(this.api.registry, abiJSONobj);
+    this.apiContract = new ApiContract(this.api, this.abi, ERC20);
 
     // assume Alice is always the user  
     const keyring = new Keyring({ type: 'sr25519' });
@@ -43,29 +45,16 @@ export class BalancesService implements OnModuleInit {
 
   async balanceOf(who: string) {
     await this.api.isReady;
-    // works but cannot get the answer
-    const result = await this.api.rpc.contracts.call(
-      {
-        dest: ERC20,
-        value: 0,
-        gasLimit: 1000000000000,
-        inputData: this.abi.messages.balanceOf(who)
-      })
-
-    return result.isSuccess;
+    const result: ContractCallOutcome = await this.apiContract.call('rpc', 'balanceOf', 0, 1000000000000, [who])
+      .send(ALICE) as ContractCallOutcome
+    return result.output.toString()
   }
 
   async totalSupply() {
     await this.api.isReady;
-    // works but cannot get the answer
-    const result = await this.api.rpc.contracts.call(
-      {
-        dest: ERC20,
-        value: 0,
-        gasLimit: 1000000000000,
-        inputData: this.abi.messages.totalSupply()
-      })
-
-    return result.isSuccess;
+    const result: ContractCallOutcome = await this.apiContract.call('rpc', 'totalSupply', 0, 1000000000000)
+      .send(ALICE) as ContractCallOutcome
+    return result.output.toString()
   }
+
 }
